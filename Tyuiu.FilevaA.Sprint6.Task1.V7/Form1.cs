6a995e9b0406ac4d63727251d604d11b72d42ca7
Полина Филeва <polina_fileva@list.ru>
2025-12-07 21:06:02 +0500

    Task 1

---
+++ Tyuiu.FilevaA.Sprint6.Task1.V7/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.FilevaA.Sprint6.Task1.V7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
