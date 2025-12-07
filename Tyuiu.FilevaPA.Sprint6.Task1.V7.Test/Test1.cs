0cb3b105f66d560eb8978039d025c20a58edf411
Полина Филeва <polina_fileva@list.ru>
2025-12-07 20:58:30 +0500

    Task 1

---
+++ Tyuiu.FilevaPA.Sprint6.Task1.V7.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.FilevaPA.Sprint6.Task1.V7.Test;
+using Tyuiu.FilevaPA.Sprint6.Task1.V7.Lib;
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
